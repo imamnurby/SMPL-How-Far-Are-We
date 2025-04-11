8541787da623626d27a369b3b946fa7b65a8f534
imamnurby <inby04@gmail.com>
2025-04-11 02:01:22 +0000

    init llm results

---
+++ emse_submission_fix/k_1/inference/uartlite/1360605874_2013-02-11_6d53c3b71d32_uartlite_ulite_get_poll_char.0.gpt4.res.c
@@ -0,0 +1,6 @@
+static int ulite_get_poll_char(struct uart_port *port)
+{
+  if (!(uart_in32(ULITE_STATUS, port) & ULITE_STATUS_RXVALID))
+    return NO_POLL_CHAR;
+  return uart_in32(ULITE_RX, port);
+}
