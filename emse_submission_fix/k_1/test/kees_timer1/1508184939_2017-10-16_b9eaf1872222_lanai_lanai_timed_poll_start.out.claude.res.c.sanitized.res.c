91c406752111e9921def080a971fb9a2a9f7cf91
imamnurby <inby04@gmail.com>
2025-04-12 04:18:06 +0000

    move test cases that are not covered in the chang examples

--- emse_submission_fix/k_1/test/kees_timer1/1508184939_2017-10-16_b9eaf1872222_lanai_lanai_timed_poll_start.out.claude.res.c.sanitized.res.c
+++
@@ -1,8 +0,0 @@
-static inline void lanai_timed_poll_start(struct lanai_dev *lanai)
-{
-  init_timer(&lanai->timer);
-  lanai->timer.expires = jiffies + LANAI_POLL_PERIOD;
-  lanai->timer.data = (unsigned long)lanai;
-  lanai->timer.function = lanai_timed_poll;
-  add_timer(&lanai->timer);
-}
--- emse_submission_fix/k_3/test/kees_timer1/1508184939_2017-10-16_b9eaf1872222_lanai_lanai_timed_poll_start.res.c
+++
@@ -1,6 +0,0 @@
-static inline void lanai_timed_poll_start(struct lanai_dev *lanai)
-{
-	setup_timer(&lanai->timer, lanai_timed_poll, (unsigned long)lanai);
-	lanai->timer.expires = jiffies + LANAI_POLL_PERIOD;
-	add_timer(&lanai->timer);
-}
